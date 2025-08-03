9c01d1d17e95bbdfa06695e3d8bc3789aa1d9c5f
ARIPerm <isanro@mail.ru>
2025-08-03 16:59:34 +0500

    added file

---
+++ Task2/Task2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+#define SUB(a,b) (a)-(b)
+
+int main(int argc, char** argv)
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int a = 6;
+	int b = 5;
+	int c = 2;
+#if defined SUB
+	std::cout << SUB(a, b) << std::endl;
+	std::cout << SUB(a, b) * c << std::endl;
+	std::cout << SUB(a, b + c) * c << std::endl;
+#else
+	std::cout << "Не определен макрос" << std::endl;
+#endif
+}
